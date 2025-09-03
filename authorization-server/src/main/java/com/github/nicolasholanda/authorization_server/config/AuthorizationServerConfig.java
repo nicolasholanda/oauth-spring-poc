4e5a1ea75d32fefe9405ec6553972fda757cb98a
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-09-02 22:17:11 -0300

    Initial commit - added authorization server

---
+++ authorization-server/src/main/java/com/github/nicolasholanda/authorization_server/config/AuthorizationServerConfig.java
@@ -0,0 +1,10 @@
+package com.github.nicolasholanda.authorization_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AuthorizationServerConfig {
+
+}
